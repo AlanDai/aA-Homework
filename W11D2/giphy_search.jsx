7e3d48de352e74b4ae932fccdbb98db0b4a0412c
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-03 13:19:29 -0800

    implement requestSearchGiphys method and connect to redux state

--- W11D2/giphy_search.jsx
+++ W11D2/giphy_search.jsx
@@ -2,3 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { fetchSearchGiphys } from './util/api_util';
+import { receiveSearchGiphys, requestSearchGiphys } from './actions/giphy_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    const store = configureStore();
+
+    // TESTING
+    window.store = store;
+    window.fetchSearchGiphys = fetchSearchGiphys;
+    window.receiveSearchGiphys = receiveSearchGiphys;
+    window.requestSearchGiphys = requestSearchGiphys;
+    // END TESTING
+
+    ReactDOM.render(<Root store={store} />, root);
+})
