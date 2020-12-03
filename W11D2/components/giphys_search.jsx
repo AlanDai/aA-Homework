6032d8fff74570a4e6cd20210dacc0c04c7df86a
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-03 13:33:16 -0800

    add container and presentational components; still need bugfix

--- W11D2/components/giphys_search.jsx
+++ W11D2/components/giphys_search.jsx
@@ -1,3 +1,37 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+export class GiphysSearch extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchTerm: ''
+        }
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleChange(e) {
+        this.setState({ searchTerm: e.currentTarget.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let searchTerm = this.state.searchTerm.split(' ').join('+');
+        this.props.fetchSearchGiphys(searchTerm)
+    }
+
+    render () {
+        return (
+            <div>
+                <form>
+                    <input value={this.state.searchTerm} onChange={this.handleChange}/>
+                    <button onClick={this.handleSubmit}>Submit Giphy</button>
+                </form>
+                <GiphysIndex giphys={giphys} />
+            </div>
+        )
+    }
+}
